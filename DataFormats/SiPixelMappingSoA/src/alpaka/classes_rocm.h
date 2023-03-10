1ecc524feb7f4db42d2b4896c0dd20a438ff3309
Dimitris Papagiannis <nothingface0@gmail.com>
2023-03-10 15:57:37 +0100

    You thought that we didn't have enough classes_def files...YOU THOUGHT WRONG

---
+++ DataFormats/SiPixelMappingSoA/src/alpaka/classes_rocm.h
@@ -0,0 +1,9 @@
+#ifndef DataFormats_SiPixelMappingSoA_Alpaka_Classes_CUDA_h
+#define DataFormats_SiPixelMappingSoA_Alpaka_Classes_CUDA_h
+
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Portable/interface/Product.h"
+#include "DataFormats/SiPixelMappingSoA/interface/SiPixelMappingLayout.h"
+#include "DataFormats/SiPixelMappingSoA/interface/alpaka/SiPixelMappingDevice.h"
+
+#endif  // DataFormats_Track_src_alpaka_classes_cuda_h
